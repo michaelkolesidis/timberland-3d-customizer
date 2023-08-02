38128e0b003cff42fda8a4bdf36fd52c66f0aea7
Michael Kolesidis <michael.kolesidis@gmail.com>
2023-08-02 15:33:28 +0300

    positioning and code cleanup

--- src/Experience.tsx
+++ src/Experience.tsx
@@ -1,11 +1,7 @@
-// import { useLoader } from "@react-three/fiber";
-// import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
 import { OrbitControls } from "@react-three/drei";
 import { Boot } from "./Boot";
 
 export default function Experience() {
-//   const boot = useLoader(GLTFLoader, "/models/boot/boot.gltf");
-
   return (
     <>
       <OrbitControls
@@ -14,12 +10,7 @@ export default function Experience() {
         minPolarAngle={Math.PI / 2}
         maxPolarAngle={Math.PI / 2}
       />
-      {/* <primitive
-        object={boot.scene}
-        position={[0, 0, 0]}
-        rotation={[0, -Math.PI / 2, 0]}
-      /> */}
-      <Boot />
+      <Boot position={[-0.5, 0, 0]} rotation={[0, -Math.PI / 2, 0]} />
     </>
   );
 }
